8c8fb55132bf7870622fdd30fa28d3f6d12d8a10
Hardik Khichi <hardikkhichi@gmail.com>
2018-06-05 18:37:15 +0530

    dummy fataflow pipeline

---
+++ dummy.py
@@ -0,0 +1,77 @@
+# Prevents program crashing with vrsion updates
+from __future__ import absolute_import
+
+# Library to pass arguments for program (input,output,runner,etc.).
+import argparse
+# Maintains log and helps in debugging. 
+import logging
+# reqular expression
+import re
+# Future allows to run program in both versions of python.
+import six
+
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+
+
+def run(argv=None):
+    """Main entry point; defines and runs the wordcount pipeline."""
+
+    #Create a parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input',
+                        dest='input',
+                        default='gs://hardik_rategain/names.csv',
+                        help='Input file to process.')
+    parser.add_argument('--output',
+                        dest='output',
+                        default='gs://hardik_rategain/output',
+                        help='Output file to write results to.')
+    known_args, pipeline_args = parser.parse_known_args(argv)
+    pipeline_args.extend([
+        # CHANGE 2/5: (OPTIONAL) Change this to DataflowRunner to
+        # run your pipeline on the Google Cloud Dataflow Service.
+        '--runner=DirectRunner',
+        # CHANGE 3/5: Your project ID is required in order to run your pipeline on
+        # the Google Cloud Dataflow Service.
+        '--project=airbnb-150508',
+        # CHANGE 4/5: Your Google Cloud Storage path is required for staging local
+        # files.
+        '--staging_location=gs://hardik_rategain/staging',
+        # CHANGE 5/5: Your Google Cloud Storage path is required for temporary
+        # files.
+        '--temp_location=gs://hardik_rategain/tmp',
+        '--job_name=job1',
+    ])
+    
+    # We use the save_main_session option because one or more DoFn's in this
+    # workflow rely on global context (e.g., a module imported at module level).
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(SetupOptions).save_main_session = True
+    with beam.Pipeline(options=pipeline_options) as p:
+        # Count the occurrences of each word.
+        counts = (
+            lines
+            | 'Split' >> (beam.FlatMap(lambda x: re.findall(r'[A-Za-z\']+', x))
+                          .with_output_types(six.text_type))
+            | 'PairWithOne' >> beam.Map(lambda x: (x, 1))
+            | 'GroupAndSum' >> beam.CombinePerKey(sum))
+
+        # Format the counts into a PCollection of strings.
+        def format_result(word_count):
+            (word, count) = word_count
+        return '%s: %s' % (word, count)
+
+        output = counts | 'Format' >> beam.Map(format_result)
+
+        # Write the output using a "Write" transform that has side effects.
+        # pylint: disable=expression-not-assigned
+        output | WriteToText(known_args.output)
+
+
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.INFO)
+  run()
